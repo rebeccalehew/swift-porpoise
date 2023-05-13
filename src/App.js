c482ff807bbeb7e043f56a4e36619a5f6be785bf
Rebecca Lehew <lehew316@gmail.com>
2023-05-12 23:44:30 -0500

    initial commit // starter code
    - Finalized file structure
    - Created files for future development

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import About from "./components/about";
+import Contact from "./components/contact";
+import Navbar from "./components/navbar";
+import Projects from "./components/projects";
+import Skills from "./components/skills";
+// import Resume from "./components/resume";
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Resume />
+      <Contact />
+    </main>
+  );
+}
---
+++ src/components/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <section id="about">
+            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                <h1 className=""
+        </section>
+    )
+}
