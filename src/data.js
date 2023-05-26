7690435183f8cf7c13bf213378f8b22bdb9d74b6
Rebecca Lehew <lehew316@gmail.com>
2023-05-25 21:42:49 -0500

    Project updates

--- src/data.js
+++ src/data.js
@@ -1,35 +1,51 @@
 export const projects = [
   {
-    title: "Employee Tracker",
-    subtitle: "Content Management System",
+    title: "Cheerful Clam",
+    subtitle: "Employee Tracker: Content Management System",
     description:
       "A command-line application managing a company's employee database, using Node.js, Inquirer, and MySQL.",
     image: "./project1.png",
     link: "https://github.com/rebeccalehew/cheerful-clam",
   },
   {
-    title: "E-Commerce App",
-    subtitle: "Backend Development",
+    title: "Embellished Crab",
+    subtitle: "E-Commerce App: Backend Development",
     description:
       "An app that builds the back end of an e-commerce site by modifying starter code. The app will configure a working Express.js API and use Sequelize to interact with a MySQL database.",
     image: "./project2.png",
     link: "https://github.com/rebeccalehew/embellished-crab",
   },
   {
-    title: "Work Day Scheduler",
-    subtitle: "A simple calendar app for scheduling throughout your work day",
+    title: "Agreeable Sea Turtle",
+    subtitle: "Work Day Scheduler",
     description:
       "The Work Day Scheduler app allows a user to save events for each hour of a typical business day. It runs int he browser and features dynamically updated HTML and CSS powered by jQuery.",
     image: "./project3.png",
     link: "https://github.com/rebeccalehew/agreeable-sea-turtle",
   },
   {
-    title: "Text Editor",
-    subtitle: "Progressive Web App",
+    title: "Luminous Jellyfish",
+    subtitle: "Text Editor PWA",
     description:
       "A text editor app that runs in the browser. The app is a single-page app that meets the PWA criteria and features a number of data persistence techniques that serve as redundancy in case one of the options is not supported by the browser. The app functions offline, utilizes IndexedDB, and is deployed using Heroku.",
     image: "./project4.png",
     link: "https://github.com/rebeccalehew/luminous-jellyfish",
+  },
+  {
+    title: "Charismatic Eel",
+    subtitle: "Professional README Generator",
+    description:
+      "A command-line application that dynamically generates a professional README.md file from a user's input using Inquirer package.",
+    image: "./project5.png",
+    link: "https://github.com/rebeccalehew/charismatic-eel",
+  },
+  {
+    title: "Bookish Narwhal",
+    subtitle: "JRR Bookstore: Full-Stack MERN app",
+    description:
+      "A retail bookstore e-commerce app with various options to interact locally.",
+    image: "./project6.png",
+    link: "https://github.com/rebeccalehew/bookish-narwhal",
   }
 ];
 
